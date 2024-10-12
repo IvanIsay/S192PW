7cc940958a1fbf1bb8856ac62086862273ce983f
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-10-11 20:31:50 -0600

    componentes y controlador

--- introLaravel/routes/web.php
+++ introLaravel/routes/web.php
@@ -1,6 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+
+
+Route::get('/', [controladorVistas::class, 'home'])->name('rutainicio');
+
+Route::get('/form', [controladorVistas::class, 'formulario'])->name('rutaform');
+
+Route::get('/clientes', [controladorVistas::class, 'consulta'])->name('rutaclientes');
+
+Route::view('/component','componentes')->name('rutacomponentes'); 
+
+
+
+
+
+
+
+
+
 
 
 //ruta de tipo GET
@@ -8,7 +27,8 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 }); */
 
-//ruta de tipo VIEW
+/* //ruta de tipo VIEW
 Route::view('/','inicio')->name('rutainicio');
 Route::view('/form','formulario')->name('rutaform');
 Route::view('/clientes','clientes')->name('rutaclientes');
+*/
